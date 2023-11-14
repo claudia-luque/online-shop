eeb5a51140fb551676b22b30b37578a75ca1af03
Claudia <claudialuque@claudias-air.home>
2023-11-14 00:07:34 +0000

    Borrando un import no necesario

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import NavBar from './components/NavBar/NavBar'
-import ItemCount from './components/ItemCount/ItemCount'
 
 
 function App() {
